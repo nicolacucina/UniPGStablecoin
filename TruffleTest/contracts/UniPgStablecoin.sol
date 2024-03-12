b4d83fb51fcd667c065d0a2163f47bfe9bf5ffb8
nicolacucina <nicola.cucina@studenti.unipg.it>
2024-03-12 17:46:58 +0100

    testing with truffle

---
+++ TruffleTest/contracts/UniPgStablecoin.sol
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import "./IERC20.sol";
+
+contract UniPGStablecoin is IERC20 {
+    /* This event is emitted when the amount of tokens (value) is sent from the from address to the to address.
+    In the case of minting new tokens, the transfer is usually from the 0x00..0000 address while in the case of burning tokens the transfer is to 0x00..0000.*/
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    /*This event is emitted when the amount of tokens (value) is approved by the owner to be used by the spender.*/
+    event Approval(
+        address indexed owner, address indexed spender, uint256 value
+    );
+
+    /*
+    ERC20InsufficientAllowance(spender, allowance, needed)
+
+    ERC20InvalidApprover(approver)
+
+    ERC20InvalidSpender(spender)*/
+
+    error RebaseInProgress();
+
+    error InitialMintingOver();
+
+    error InsufficientBalance(uint requested, uint available);
+
+    error InvalidSender(address sender);
+
+    error InvalidReceiver(address receiver);
+
+    uint256 public totalSupply;
+    bool public isRebasing;
+    bool public initialMinting;
+    mapping(address => uint256) public balanceOf;
+    address[] public walletAddresses;
+    mapping(address => mapping(address => uint256)) public allowance;
+    string public name;
+    string public symbol;
+    uint8 public decimals; 
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        isRebasing = false;
+        initialMinting = true;
+    }
+        
+    /* this is used only in the inital phase of the contract where tokens are given to initial backers.*/
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == address(0));
+        require(initialMinting == true);
+        walletAddresses.push(to);
+        _mint(to, amount);
+    }
+
+    function blockMinting() external {
+        require(msg.sender == address(0));
+        require(initialMinting == true);
+        initialMinting = false;
+    }
+
+    function _mint(address to, uint256 amount) internal {
+        balanceOf[to] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal {
+        balanceOf[from] -= amount;
+        totalSupply -= amount;
+        emit Transfer(from, address(0), amount);
+    }
+
+    function _rebase() internal {
+        isRebasing = true;
+        uint16 price = 105;
+        
+        /*for loops are not defined for mappings, the workaround is that when a key is added to a mapping,
+        it also added to an array */
+        for(uint i = 0; i < walletAddresses.length; i++) {
+            /* fastest method, but using mint and burn triggers Transfer event
+            balanceOf[account] = balanceOf[account] * price; */
+
+            uint256 diff = balanceOf[walletAddresses[i]] - balanceOf[walletAddresses[i]] * price; 
+            if(price > 1){
+                _mint(walletAddresses[i], diff);
+            }else if(price < 1){
+                _burn(walletAddresses[i], diff);
+            }else{
+                // do nothing
+            }
+        }
+        isRebasing = false;
+    }
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool)
+    {   
+        require(isRebasing == false);
+        if (amount > balanceOf[msg.sender]){
+            revert InsufficientBalance({
+                requested: amount,
+                available: balanceOf[msg.sender]
+            });
+        }                
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        walletAddresses.push(recipient);
+        emit Transfer(msg.sender, recipient, amount);
+        return true;        
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount)
+        external
+        returns (bool)
+    {
+        if(isRebasing){
+            return false; 
+        }else{
+            if (amount > balanceOf[msg.sender]){
+                revert InsufficientBalance({
+                    requested: amount,
+                    available: balanceOf[msg.sender]
+                });
+            }
+            allowance[sender][msg.sender] -= amount;
+            balanceOf[sender] -= amount;
+            balanceOf[recipient] += amount;
+            emit Transfer(sender, recipient, amount);
+            return true;
+        }
+    }
+}
