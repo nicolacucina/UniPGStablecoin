bed7208cabf81469906eb146e1166a9aa3a384d1
nicolacucina <nicola.cucina@studenti.unipg.it>
2024-04-22 17:31:10 +0200

    recommitting all to ensure that everything is updated

--- TruffleTest/evolution.bat
+++ TruffleTest/evolution.bat
@@ -1,8 +1,2 @@
-REM Run price simulation
-REM truffle exec priceEvolution.js
-
 REM Run general simulation
 truffle exec simulation.js
-
-REM Open the browser
-REM start msedge http://localhost:5500
